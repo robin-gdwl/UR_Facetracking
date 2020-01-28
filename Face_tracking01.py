56c8cb0f135066b19902c557a94601145e7d9e8b
Robin Godwyll <rodwyll+github@gmail.com>
2020-01-28 22:32:37 +0100

    scaling tests 01

--- Face_tracking01.py
+++ Face_tracking01.py
@@ -1,10 +1,19 @@
 import URBasic
+import math
 import sys
 import cv2
 import time
 from imutils.video import VideoStream
 
+# imports for visualisation purposes
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.ylim(-100,100)
+plt.xlim(-100,100)
+ax.set_zlim(-100,100)
 
 
 # Setup and variables
@@ -31,6 +40,8 @@ vs = VideoStream(usePiCamera= RASPBERRY_BOOL,
                               rotation = 180).start()
 time.sleep(0.2)
 
+angle_multiplier = 0.01
+
 """host = '192.168.0.113'   #E.g. a Universal Robot offline simulator, please adjust to match your IP
 acc = 0.9
 vel = 0.9
@@ -45,19 +56,34 @@ def find_faces_in_frame():
     frame = vs.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    #TODO: make it so that it is not as important if a face is not recognised in 1 or 2 frames
     for (x, y, w, h) in faces:
         face_center = (int(x+ w*0.5), int(y+ h*0.5))
         position_from_center = [face_center[0] - video_midpoint[0] , face_center[1] - video_midpoint[1]]
 
 
         list_of_facepos.append(face_center)
+        # draw information about the face on the frame
+        cv2.putText(frame, str(position_from_center), face_center, 0, 1, (0,200,0))
         cv2.line(frame, video_midpoint, face_center, (0,200,0), 5)
         cv2.circle(frame, face_center, 4, (0,200,0), 3)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
     cv2.imshow('img', frame)
-    k = cv2.waitKey(30) & 0xff
-    print(list_of_facepos)
+    k = cv2.waitKey(10) & 0xff
+    #print(list_of_facepos)
     return list_of_facepos
+
+def convert_robot_target(rob_target):
+    x_val = rob_target[0]
+    y_val = rob_target[1]
+    t = math.asin(x_val / sphere_radius)
+    s = math.asin(y_val / sphere_radius)
+
+    x = sphere_radius * math.cos(s) * math.sin(t)
+    y = sphere_radius * math.sin(s) * math.sin(t)
+    z = sphere_radius * math.cos(t)
+
+    return [x, y, z]
 
 
 
@@ -66,14 +92,28 @@ def find_faces_in_frame():
 #robot.movej(q=[-3.14,-1.,0.5, -1.,-1.5,0], a=acc, v=vel)
 #time.sleep(20)
 
-robot_position = [0,0]
+robot_position = [0,0]  # Robot Position as Angles in Radians
+sphere_center = [0,0,0]
+sphere_radius = 100
 
+i = 0
 while True:
 
     face_positions = find_faces_in_frame()
     if len(face_positions) > 0:
-        face = face_positions[0]
-
+        face = list(face_positions[0])
+        #print(face)
+        face[:] = [x * angle_multiplier for x in face]
+        #print(face)
+        #print()
+        robot_target = [a+b for a,b in zip(robot_position,face)]
+        print(robot_target)
+        robot_position = robot_target
+        x,y,z = convert_robot_target(robot_target)
+        ax.scatter(x, y, z, marker="o")
+    i+=1
+    if i >=100:
+        plt.show()
 
 
 
